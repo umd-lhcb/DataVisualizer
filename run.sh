4fcdff2b33b9a9125bb263cedf08b83137d112e2
Yipeng Sun <syp@umd.edu>
2018-08-06 00:20:48 -0400

    We also need to allow access to the websocket from anywhere.

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #
-# Last Change: Mon Aug 06, 2018 at 12:14 AM -0400
+# Last Change: Mon Aug 06, 2018 at 12:20 AM -0400
 
-bokeh serve DVApp --address 0.0.0.0 --port 56789
+bokeh serve DVApp --address 0.0.0.0 --port 56789 --allow-websocket-origin=*
