865074448f811f0244445cb8f48c2bddfc01fc9f
Yipeng Sun <syp@umd.edu>
2018-08-14 16:21:06 -0400

    Add a note on how to pass-thru command line arguments.

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 #
-# Last Change: Mon Aug 06, 2018 at 12:20 AM -0400
+# Last Change: Tue Aug 14, 2018 at 04:21 PM -0400
+
+# NOTE: we could use '--arg' flag to pass-thru arguments to boken app.
 
 bokeh serve DVApp --address 0.0.0.0 --port 56789 --allow-websocket-origin=*
