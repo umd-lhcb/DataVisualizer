851ee0427ca2b0c3d91ec4bde026468383dff281
Yipeng Sun <syp@umd.edu>
2018-08-15 02:07:33 -0400

    Pass cli arguments to the app.

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-# Last Change: Tue Aug 14, 2018 at 04:21 PM -0400
+# Last Change: Wed Aug 15, 2018 at 01:57 AM -0400
 
-# NOTE: we could use '--arg' flag to pass-thru arguments to boken app.
-
-bokeh serve DVApp --address 0.0.0.0 --port 56789 --allow-websocket-origin=*
+# NOTE: we could use '--args' flag to pass-thru arguments to boken app.
+bokeh serve DVApp --address 0.0.0.0 --port 56789 --allow-websocket-origin=* \
+    --args "$@"
