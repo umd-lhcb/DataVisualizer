8399195aa3928e029bf98c439eeede265ae8e391
Yipeng Sun <syp@umd.edu>
2020-06-23 21:26:55 +0800

    Add a nix-shell config.

---
+++ shell.nix
@@ -0,0 +1,39 @@
+let
+  pkgs = import <nixpkgs> {};
+  python = pkgs.python3;
+  pythonPackages = python.pkgs;
+in
+
+pkgs.mkShell {
+  name = "DataVisualizer";
+  buildInputs = with pythonPackages; [
+    # Compilers and other build dependencies
+    #pkgs.gcc
+
+    # Some Python libraries needs to be installed via nix
+    hidapi
+    RPi_GPIO
+
+    # Python requirements (enough to get a virtualenv going).
+    virtualenvwrapper
+  ];
+
+  shellHook = ''
+    # Allow the use of wheels.
+    SOURCE_DATE_EPOCH=$(date +%s)
+
+    if test -d $HOME/build/python-venv; then
+      VENV=$HOME/build/python-venv/DataVisualizer
+    else
+      VENV=./.virtualenv
+    fi
+
+    if test ! -d $VENV; then
+      virtualenv $VENV
+    fi
+    source $VENV/bin/activate
+
+    # allow for the environment to pick up packages installed with virtualenv
+    export PYTHONPATH=$VENV/${python.sitePackages}/:$PYTHONPATH
+  '';
+}
